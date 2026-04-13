bf2f7caaebb89b5715066eb4d6c422f1df38d36a
Qian Gong <gongqian.de@gmail.com>
2026-04-13 21:57:36 +0200

    feat(destination-details): display destination details based on slug

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -1,17 +1,20 @@
-import travelDatas from '@/data/travel.json';
+import { Link } from 'react-router';
+import travelData from '@/data/travel.json';
 
 const Destinations = () => {
   return (
     <div className="p-6 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-      {travelDatas.map((item) => (
+      {travelData.map((item) => (
         <div key={item.slug} className="card bg-base-100 shadow">
-          <figure>
-            <img src={item.image} alt={item.name} />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">{item.name}</h2>
-            <p>{item.description}</p>
-          </div>
+          <Link to={`/destinations/${item.slug}`}>
+            <figure>
+              <img src={item.image} alt={item.title} />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{item.title}</h2>
+              <p>{item.description}</p>
+            </div>
+          </Link>
         </div>
       ))}
     </div>
