8ca2ae42cc1149825ae8829b70556b21a9db1e4d
Qian Gong <gongqian.de@gmail.com>
2026-04-13 19:04:36 +0200

    feat(router): add pages and route configuration

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+// src/pages/index.js
+export { default as Home } from './Home';
+export { default as Destinations } from './Destinations';
+export { default as DestinationDetails } from './DestinationDetails';
+export { default as About } from './About';
+export { default as Contact } from './Contact';
+export { default as NotFound } from './NotFound';
