5c22c7549ed1e975446ce89bad204fb06c54ef5b
Qian Gong <gongqian.de@gmail.com>
2026-04-13 23:05:20 +0200

    fix(router): ensure router context wraps app components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from 'react-router';
+import { Routes, Route } from 'react-router';
 
 import Navbar from '@/components/layout/Navbar';
 import Footer from '@/components/layout/Footer';
@@ -18,19 +18,14 @@ const App = () => {
       <Navbar />
 
       <main className="grow">
-        <BrowserRouter basename="/travel-agency">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/destinations" element={<Destinations />} />
-            <Route
-              path="/destinations/:slug"
-              element={<DestinationDetails />}
-            />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destinations" element={<Destinations />} />
+          <Route path="/destinations/:slug" element={<DestinationDetails />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </main>
 
       <Footer />
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './styles/main.css';
 import App from './App.jsx';
 
+import { BrowserRouter } from 'react-router';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/travel-agency">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
